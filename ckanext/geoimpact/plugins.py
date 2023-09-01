da168cedabb0230d83f1ec22483ec86d8b9b7b0f
Thilo Weber <thilo.weber@geoimpact.ch>
2023-09-01 15:26:37 +0200

    wip

--- ckanext/geoimpact/plugins.py
+++ ckanext/geoimpact/plugins.py
@@ -1,7 +1,8 @@
 from ckan import plugins as p
 import ckan.lib.mailer as mailer
-from ckan.common import CKANConfig
+# from ckan.common import CKANConfig
 from ckan.plugins import IAuthFunctions, toolkit
+from ckan.common import _, CKANConfig
 
 from .patches.emails import send_reset_link, send_invite
 from .utils.auth_functions import user_list
@@ -10,6 +11,7 @@ from .utils.auth_functions import user_list
 class GeoimpactPlugin(p.SingletonPlugin):
     p.implements(p.IConfigurer, inherit=True)
     p.implements(IAuthFunctions, inherit=True)
+    p.implements(p.IFacets, inherit=True)
 
     # Monkey patches for emails to use custom templates
     mailer.send_reset_link = send_reset_link
@@ -36,3 +38,13 @@ class GeoimpactPlugin(p.SingletonPlugin):
         }
 
         return auth_functions
+
+    # IFacets
+    def dataset_facets(self, facets_dict, package_type):
+        """
+        This function is called by ckan to get the dataset facets.
+        We overwrite the default dataset facets here
+        """
+        facets_dict['categories'] = _('Categories')
+        return facets_dict
+
